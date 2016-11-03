f3c9c200225dd4bc58c32cb38e5fc69a827b791e
Caramel <luotianyi@Luos-MacBook-Pro.local>
2016-11-03 02:21:21 -0700

    Naive bayes classification code version 1.0

--- preprocess_yelp_dataset.py
+++ preprocess_yelp_dataset.py
@@ -3,14 +3,19 @@ import json
 if __name__ == '__main__':
     dataset_file_location = '/Users/luotianyi/Desktop/CMPS242/project/'\
     'yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
-    f_write_dataset = open('/Users/luotianyi/Desktop/CMPS242/project/'\
-    'trainingdata_20161102', 'w')
+    f_write_dataset_train = open('/Users/luotianyi/Desktop/CMPS242/project/'\
+    'traindata_20161103', 'w')
+    f_write_dataset_test = open('/Users/luotianyi/Desktop/CMPS242/project/'\
+    'testdata_20161103', 'w')
     #print dataset_file_location
     line_num_int = 0
+    line_valid_data = 0
     for line in open(dataset_file_location, 'r'):
         line_num_int += 1
         if line_num_int % 5000 == 0:
-            print str(line_num_int) + " lines are handled"
+            print str(line_num_int) + " lines are handled"      
+        if line_valid_data == 50000:
+            break
         temp_result_str = ''
         #print line
         data = json.loads(line)
@@ -19,10 +24,18 @@ if __name__ == '__main__':
         text_lower_string = data['text'].lower()
         rating_int = int(data['stars'])
         if rating_int == 4 or rating_int == 5:
+            line_valid_data += 1
             temp_result_str = '1' + '\t' +text_lower_string.replace("\n","") + '\n'
-            f_write_dataset.write(temp_result_str.encode('utf-8'))
+            if line_valid_data <= 40000:
+                f_write_dataset_train.write(temp_result_str.encode('utf-8'))
+            else:
+                f_write_dataset_test.write(temp_result_str.encode('utf-8'))
         if rating_int == 0 or rating_int == 1:
+            line_valid_data += 1
             temp_result_str = '0' + '\t' +text_lower_string.replace("\n","") + '\n'
-            f_write_dataset.write(temp_result_str.encode('utf-8'))
+            if line_valid_data <= 40000:
+                f_write_dataset_train.write(temp_result_str.encode('utf-8'))
+            else:
+                f_write_dataset_test.write(temp_result_str.encode('utf-8'))
     
     
---
+++ word_dictionary.py
@@ -0,0 +1,57 @@
+'''
+Created on 2 Nov, 2016
+
+@author: luotianyi
+'''
+import json
+
+if __name__ == '__main__':
+    dataset_file_location = '/Users/luotianyi/Desktop/CMPS242/project/'\
+    'traindata_20161103'
+    f_write_dataset_pos = open('/Users/luotianyi/Desktop/CMPS242/project/'\
+    'word_pos_frequency_40000_20161102', 'w')
+    f_write_dataset_neg = open('/Users/luotianyi/Desktop/CMPS242/project/'\
+    'word_neg_frequency_40000_20161102', 'w')
+    #print dataset_file_location
+    line_num_int = 0 #number of line
+    word_diction_pos = {} #word dictionary of positive samples
+    word_diction_neg = {} #word dictionary of positive samples
+    word_pos_num = 0 #number of words in the positive samples
+    word_neg_num = 0 #number of words in the negative samples
+    word_distinct_pos_num = 0 #number of distinct words in the positive samples
+    word_distinct_neg_num = 0 #number of distinct words in the negtive samples
+    sentence_pos_num = 0 #number of positive samples
+    sentence_neg_num = 0 #number of negtive samples
+    for line in open(dataset_file_location, 'r'):
+        line_num_int += 1
+        if line_num_int % 5000 == 0:
+            print str(line_num_int) + " lines are handled"
+        temp_str_arraylist = line.split("\t")
+        temp_text = temp_str_arraylist[1].replace("\n", "")
+        temp_label = int(temp_str_arraylist[0])
+        temp_text_arraylist = temp_text.split(" ")
+        if temp_label == 1:
+            sentence_pos_num += 1
+        else:
+            sentence_neg_num += 1
+        for word in temp_text_arraylist:
+            if temp_label == 1:
+                word_pos_num += 1
+                if word + "_pos" not in word_diction_pos:
+                    word_distinct_pos_num +=1
+                    word_diction_pos[word + "_pos"] = 1
+                else:
+                    word_diction_pos[word + "_pos"] += 1
+            else:
+                word_neg_num += 1
+                if word + "_neg" not in word_diction_neg:
+                    word_distinct_neg_num +=1
+                    word_diction_neg[word + "_neg"] = 1
+                else:
+                    word_diction_neg[word + "_neg"] += 1
+    f_write_dataset_pos.write(str(sentence_pos_num) + "\t" + str(word_distinct_pos_num) + "\t" + str(word_pos_num) + "\n")
+    for word_write_pos in word_diction_pos.keys():    
+        f_write_dataset_pos.write(word_write_pos + "\t" + str(word_diction_pos[word_write_pos]) + "\n")
+    f_write_dataset_neg.write(str(sentence_neg_num) + "\t" + str(word_distinct_neg_num) + "\t" + str(word_neg_num) + "\n")
+    for word_write_neg in word_diction_neg.keys(): 
+        f_write_dataset_neg.write(word_write_neg + "\t" + str(word_diction_neg[word_write_neg]) + "\n")
