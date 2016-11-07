02f68439a53cb54dba374974b8ec79b6f1b795ee
pkuluotianyi <pkuluotianyi@gmail.com>
2016-11-06 20:03:11 -0800

    Update preprocess_yelp_dataset.py

--- preprocess_yelp_dataset.py
+++ preprocess_yelp_dataset.py
@@ -3,6 +3,8 @@ import json
 if __name__ == '__main__':
     dataset_file_location = '/Users/luotianyi/Desktop/CMPS242/project/'\
     'yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
+    stopwords_file = '/Users/luotianyi/Desktop/Develope/workspace/cmps242project'\
+    '/google_stopwords'
     f_write_dataset_train = open('/Users/luotianyi/Desktop/CMPS242/project/'\
     'traindata_20161103', 'w')
     f_write_dataset_test = open('/Users/luotianyi/Desktop/CMPS242/project/'\
@@ -10,6 +12,10 @@ if __name__ == '__main__':
     #print dataset_file_location
     line_num_int = 0
     line_valid_data = 0
+    stop_words_list = []
+    for temp_word in open(stopwords_file, 'r'):
+        stop_words_list.append(str(temp_word.replace("\n","")))
+    
     for line in open(dataset_file_location, 'r'):
         line_num_int += 1
         if line_num_int % 5000 == 0:
@@ -25,17 +31,39 @@ if __name__ == '__main__':
         rating_int = int(data['stars'])
         if rating_int == 4 or rating_int == 5:
             line_valid_data += 1
-            temp_result_str = '1' + '\t' +text_lower_string.replace("\n","") + '\n'
-            if line_valid_data <= 40000:
+            #temp_result_str = '1' + '\t' + text_lower_string.replace("\n","") + '\n'
+            temp_stopwords_list = text_lower_string.replace("\n","").split(" ")
+            temp_string = ""
+            temp_index = 0
+            for temp_word in temp_stopwords_list:
+                temp_index += 1
+                if temp_index == len(temp_stopwords_list):
+                    if temp_word not in stop_words_list:
+                        temp_string += temp_word + "\n"
+                else: 
+                    if temp_word not in stop_words_list:
+                        temp_string += temp_word + " "
+            temp_result_str = '1' + '\t' + temp_string
+            if line_valid_data <= 1711125:
                 f_write_dataset_train.write(temp_result_str.encode('utf-8'))
             else:
                 f_write_dataset_test.write(temp_result_str.encode('utf-8'))
         if rating_int == 0 or rating_int == 1:
             line_valid_data += 1
             temp_result_str = '0' + '\t' +text_lower_string.replace("\n","") + '\n'
-            if line_valid_data <= 40000:
+            temp_stopwords_list = text_lower_string.replace("\n","").split(" ")
+            temp_string = ""
+            temp_index = 0
+            for temp_word in temp_stopwords_list:
+                temp_index += 1
+                if temp_index == len(temp_stopwords_list):
+                    if temp_word not in stop_words_list:
+                        temp_string += temp_word + "\n"
+                else: 
+                    if temp_word not in stop_words_list:
+                        temp_string += temp_word + " "
+            temp_result_str = '1' + '\t' + temp_string
+            if line_valid_data <= 1711125:
                 f_write_dataset_train.write(temp_result_str.encode('utf-8'))
             else:
                 f_write_dataset_test.write(temp_result_str.encode('utf-8'))
-    
-    
